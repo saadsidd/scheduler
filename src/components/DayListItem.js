1628e585be0b6cb136d400fa2a267f1227c32701
Saad Siddiq <siddiq.saad@gmail.com>
2022-07-14 01:53:43 -0500

    displays correct message based on available spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,15 +4,21 @@ import classNames from "classnames";
 import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
-  let dayClass = classNames("day-list__item", {
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots
   });
 
+  const formatSpots = function() {
+    if (props.spots === 0) return 'no spots';
+    if (props.spots === 1) return '1 spot';
+    return `${props.spots} spots`;
+  }
+
   return (
     <li className={dayClass} onClick={() => {props.setDay(props.name)}}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()} remaining</h3>
     </li>
   );
 }
